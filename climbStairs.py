a641bae3458a460c89a7589e1bcaeda669d3b1a3
hatcherfang <hatcherfang@163.com>
2017-07-10 14:12:02 +0800

    climbStairs

---
+++ climbStairs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        return self.climbStairs(n-1) + self.climbStairs(n-2)
+if __name__ == '__main__':
+    cs = Solution()
+    print cs.climbStairs(35)
